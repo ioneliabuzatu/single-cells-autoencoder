2f2c09e059b892ec8839942cf07d2ef7fbf8ac65
ioneliabuzatu <buzatuionelia@gmail.com>
2022-09-26 23:46:49 +0200

    autoencoder_batch#100

---
+++ config.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import MinMaxScaler
+
+batch_size = 100
+
+norms_types = {"minmax": lambda x: MinMaxScaler().fit_transform(x)}
+norm = "nope"
+# norm = "minmax"
+# norm = "standardization" 
