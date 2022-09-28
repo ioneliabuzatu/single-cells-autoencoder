793d50d5c59ed57e0055c327fc146935d34d7db7
ioneliabuzatu <buzatuionelia@gmail.com>
2022-09-28 13:23:13 +0200

    autoencoder_batch#100

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn, optim
+import numpy as np
+from matplotlib import pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+from sklearn.datasets import make_swiss_roll
+from sklearn.preprocessing import MinMaxScaler
+
+
+class Autoencoder(nn.Module):
+    """
+    in_shape [int] : input shape
+    enc_shape [int] : desired encoded shape
+    """
+
+    def __init__(self, in_shape, enc_shape):
+        super(Autoencoder, self).__init__()
+
+        self.encode = nn.Sequential(
+            nn.Linear(in_shape, 128),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(64, enc_shape),
+        )
+        # # nn.BatchNorm1d(enc_shape),
+        self.decode = nn.Sequential(
+            nn.Linear(enc_shape, 64),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(64, 128),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(128, in_shape)
+        )
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.decode(x)
+        return x
