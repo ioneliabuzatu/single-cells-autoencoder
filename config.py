6b06da2ef83856aeb46fdcc367f4305e2b148653
ioneliabuzatu <buzatuionelia@gmail.com>
2022-10-02 22:26:11 +0200

    MinMaxX#batchNORM+SELU#autoencoder_batch#100

---
+++ config.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+batch_size = 100
+
+norms_types = {"minmax": lambda x: MinMaxScaler().fit_transform(x), "standardization": None}
+norm = "nope"
+# norm = "minmax"
+# norm = "standardization" 
+
+# Scale data to have zero mean and unit variance
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+# x_train = scaler.transform(X_train)
