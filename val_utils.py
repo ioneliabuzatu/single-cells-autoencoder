118899438f1a3f6cf91dbe2cd2b46ba019793b27
ioneliabuzatu <buzatuionelia@gmail.com>
2022-10-03 21:57:07 +0200

    [CLUSTER] latent10

--- val_utils.py
+++ val_utils.py
@@ -16,8 +16,8 @@ def val(run, epoch, val_loader, device, autoencoder, loss_fn_autoencoder, regres
         for x_id, (x_batch, y_batch) in enumerate(val_loader):
             x = x_batch.to(device)
             y = y_batch.to(device)
-            encoded = autoencoder.encode(x)
-            decoded = autoencoder.decode(encoded)
+            encoded = autoencoder.encoder(x)
+            decoded = autoencoder.decoder(encoded)
             mse_autoencoder = loss_fn_autoencoder(decoded, x).item()
             epoch_val_mse_autoencoder.append(mse_autoencoder)
             del decoded
