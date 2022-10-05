3ab618ce64b16a655daa453b7d91050e95fbfe59
ioneliabuzatu <buzatuionelia@gmail.com>
2022-10-05 20:32:54 +0200

    !!!multi_regressors...#COMP100b200

--- autoencoder_multiple_regressors.py
+++ autoencoder_multiple_regressors.py
@@ -15,7 +15,8 @@ from torch import nn, optim
 from torch.utils.data import Dataset, DataLoader
 
 import config
-from model import Autoencoder, Regressor, AutoencoderV2, VariationalAutoencoder, AutoencoderV3
+from model import Autoencoder, AutoencoderV2, VariationalAutoencoder, AutoencoderV3
+from model import SingleFeatureRegressor
 from utils import correlation_score_fn, DatasetWithY
 from val_utils import val
 
@@ -36,7 +37,7 @@ buddy = experiment_buddy.deploy(
         'project': 'kaggle',
         'reinit': False
     },
-    wandb_run_name=f"!!!V2...#COMP{config.latent_space}b{config.batch_size}",
+    wandb_run_name=f"!!!multi_regressors...#COMP{config.latent_space}b{config.batch_size}",
     extra_modules=["cuda/11.1/nccl/2.10", "cudatoolkit/11.1", "cuda/11.1/cudnn/8.1"]
 )
 
@@ -72,31 +73,8 @@ else:
     y_train = cite_train_targets[:50_000]
     y_val = cite_train_targets[50_000:]
 
-
-class MyDataset(Dataset):
-
-    def __init__(self, inputs, train: bool):
-        if type(inputs) == pd.DataFrame: inputs = inputs.values
-
-        if whoami == 'ionelia':
-            if train:
-                x = inputs[:50]
-            else:
-                x = inputs[50:]
-        else:
-            if train:
-                x = inputs[:50_000]
-            else:
-                x = inputs[50_000:]
-
-        self.x = torch.tensor(x, dtype=torch.float32)
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, idx):
-        return self.x[idx]
-
+autoencoder = Autoencoder(in_shape=22050, enc_shape=config.latent_space).float().to(device)
+multiple_regressors = [SingleFeatureRegressor(in_shape=config.latent_space).float().to(device) for _ in range(140)]
 
 # cite_train_inputs = MinMaxScaler().fit_transform(cite_train_inputs)
 # cite_train_inputs = StandardScaler().fit_transform(cite_train_inputs)
@@ -105,25 +83,17 @@ train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=T
 val_dataset = DatasetWithY(cite_train_inputs, cite_train_targets, train=False, whoami=whoami)
 val_loader = DataLoader(val_dataset, batch_size=10, shuffle=False, drop_last=False)
 
-# autoencoder = VariationalAutoencoder(config.latent_space, device).to(device)
-# autoencoder = Autoencoder(in_shape=22050, enc_shape=config.latent_space).float().to(device)
-autoencoder = AutoencoderV2(in_shape=22050, enc_shape=config.latent_space).float().to(device)
-regressor = Regressor(in_shape=config.latent_space).float().to(device)
+
 print("how much does the model weight?")
 os.system('nvidia-smi')
 loss_fn_autoencoder = nn.MSELoss(reduction='sum')
-loss_fn_regressor = nn.MSELoss(reduction='sum')
-# loss_fn_total = nn.MSELoss(reduction='sum')
+# loss_fn_regressor = nn.MSELoss(reduction='sum')
+multiple_loss_fn_regressors = [nn.MSELoss(reduction='sum') for _ in range(140)]
 cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
 optimizer_autoencoder = optim.Adam(autoencoder.parameters(), lr=config.lr)
-# optimizer_encoder = optim.Adam(autoencoder.encoder.parameters())
-# optimizer_decoder = optim.Adam(autoencoder.decoder.parameters())
-optimizer_regressor = optim.Adam(regressor.parameters())
-# optimizer_autoencoder = optim.SGD(autoencoder.parameters(), lr=0.01, momentum=0.95)
-# optimizer_regressor = optim.SGD(regressor.parameters(), lr=0.01, momentum=0.95)
-
-# optimizer_total = optim.Adam(list(autoencoder.parameters() + regressor.parameters()))
+# optimizer_regressor = optim.Adam(regressor.parameters())
+optimizers_multiple_regressors = [optim.Adam(regressor.parameters()) for regressor in multiple_regressors]
 
 lambda_scheduler = lambda epoch: 0.85 ** epoch
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer_autoencoder, lr_lambda=lambda_scheduler)
@@ -138,7 +108,7 @@ curr_step_train = 0
 for epoch in range(1, n_epochs):
     logging.info(f"Epoch {epoch:10}")
     autoencoder.train()
-    regressor.train()
+    multiple_regressors = list(map(lambda model: model.train(), multiple_regressors))
     losses_epoch = []
     losses_epoch_regressor = []
     pearson_epoch_regressor = []
@@ -161,16 +131,23 @@ for epoch in range(1, n_epochs):
         # optimizer_decoder.step()
         losses_epoch.append(loss_autoencoder.item())
 
-        # train regressor
-        optimizer_regressor.zero_grad()
+        # train multiple regressors
+        for optim_regressor in optimizers_multiple_regressors:
+            optim_regressor.zero_grad()
         reduced_x = torch.tensor(reduced_x, requires_grad=True)
-        protein_predictions = regressor(reduced_x)
-        loss_regressor = loss_fn_regressor(protein_predictions, y)
-        pearson_loss_regressor = cos(y - y.mean(dim=1, keepdim=True), protein_predictions - protein_predictions.mean(dim=1, keepdim=True)).mean()
-        pearson_epoch_regressor.append(pearson_loss_regressor.item())
-        loss_regressor.backward()
-        optimizer_regressor.step()
-        losses_epoch_regressor.append(loss_regressor.item())
+
+        protein_predictions = list(map(lambda model: model(reduced_x), multiple_regressors))
+        # protein_predictions = torch.stack(protein_predictions, dim=1).squeeze()
+        losses_regressors = list(map(lambda loss, pred, true: loss(pred, true), multiple_loss_fn_regressors, protein_predictions, y.T))
+        # loss_regressor = loss_fn_regressor(protein_predictions, y)
+        # pearson_loss_regressor = cos(y - y.mean(dim=1, keepdim=True), protein_predictions - protein_predictions.mean(dim=1, keepdim=True)).mean()
+        # pearson_epoch_regressor.append(pearson_loss_regressor.item())
+        for loss_regressor in losses_regressors:
+            loss_regressor.backward()
+        for optim_regressor in optimizers_multiple_regressors:
+            optim_regressor.step()
+
+        # losses_epoch_regressor.append(loss_regressor.item())
 
         del x
         del reduced_x
@@ -181,17 +158,17 @@ for epoch in range(1, n_epochs):
     lr_autoencoder = optimizer_autoencoder.param_groups[0]["lr"]
     run.log({"train/lr_autoencoder": lr_autoencoder, 'epoch': epoch})
 
-    losses_epoch_regressor = np.array(losses_epoch_regressor)
-    pearson_epoch_regressor = np.array(pearson_epoch_regressor)
-    run.log({"train/regressor_mse_epoch": losses_epoch_regressor.mean(), 'epoch': epoch})
-    run.log({"train/pearson_regressor": pearson_epoch_regressor.mean(), 'epoch':epoch})
-    print(f"train/pearson:{pearson_epoch_regressor.mean():.3f}")
+    # losses_epoch_regressor = np.array(losses_epoch_regressor)
+    # pearson_epoch_regressor = np.array(pearson_epoch_regressor)
+    # run.log({"train/regressor_mse_epoch": losses_epoch_regressor.mean(), 'epoch': epoch})
+    # run.log({"train/pearson_regressor": pearson_epoch_regressor.mean(), 'epoch':epoch})
+    # print(f"train/pearson:{pearson_epoch_regressor.mean():.3f}")
 
     del losses_epoch
     del losses_epoch_regressor
     del pearson_epoch_regressor
 
-    if epoch % 10 == 0:
+    if epoch % 5 == 0:
         scheduler.step()
 
     # ---------------------------------------- eval ------------------------------------------------- #
@@ -201,9 +178,11 @@ for epoch in range(1, n_epochs):
         device,
         autoencoder,
         loss_fn_autoencoder,
-        regressor=regressor,
-        loss_fn_regressor=loss_fn_regressor,
+        # regressor=regressor,
+        # loss_fn_regressor=loss_fn_regressor,
+        regressor=multiple_regressors,
+        loss_fn_regressor=multiple_loss_fn_regressors,
         y_val=y_val
     )
 
-torch.save(autoencoder.state_dict(), f"{base_dir}/checkpoints/comp_autoencoder.pth")
+    torch.save(autoencoder.state_dict(), f"{base_dir}/checkpoints/comp_autoencoder.pth")
